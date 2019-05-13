ef5a0d1f18d629d9d28dee3b9c14078cdbf54b2c
mimseong <parkseongmin98@gmail.com>
2019-05-13 15:33:47 +0900

    ing

---
+++ 함수사용하기/self_number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String args[]){
+		d(100);
+	}
+
+	public int d(int n){
+		if(n <= 0)
+			return;
+		else{
+			return d(n) + d
+		}
+	}
+}
