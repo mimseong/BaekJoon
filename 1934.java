b6e25a55b5de3a4956d5d4ea1d76b44bb642076c
mimseong <parkseongmin98@gmail.com>
2019-05-16 23:45:52 +0900

    최소공배수

---
+++ 1934.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String args[]){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n1, n2, result;
+		StringTokenizer st;
+		
+		try{
+   		 	int inputNum = Integer.parseInt(br.readLine());
+			for(int i = 0; i < inputNum; i++){
+				st = new StringTokenizer(br.readLine());
+				n1 = Integer.parseInt(st.nextToken());
+				n2 = Integer.parseInt(st.nextToken());
+
+				result = LCM(n1, n2);
+
+				bw.write(Integer.toString(result));
+				bw.newLine();
+			}
+			br.close();
+			bw.close();
+		} catch (IOException e) {
+    		e.printStackTrace();
+		} 	
+	}
+
+	public static int LCM(int n1, int n2){
+		return ((n1 * n2) / GCD(n1, n2));
+	}
+
+	public static int GCD(int n1, int n2){
+		if(n2 == 0)
+			return n1;
+		return GCD(n2, n1 % n2);
+	}
+}
