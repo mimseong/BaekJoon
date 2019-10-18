4b03da2a828c752a2f8a4c3fca95f16477120e0c
mimseong <parkseongmin98@gmail.com>
2019-10-19 08:09:33 +0900

    feat: Add today's problem 10430, 10869, 10998, 11654

---
+++ 10430.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n", (a+b)%c);
+    printf("%d\n", (a%c + b%c)%c);
+    printf("%d\n", (a*b)%c);
+    printf("%d\n", (a%c * b%c)%c);
+}
---
+++ 10869.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a+b);
+    printf("%d\n", a-b);
+    printf("%d\n", a*b);
+    printf("%d\n", a/b);
+    printf("%d", a%b);
+}
