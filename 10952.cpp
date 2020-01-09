8b80796b37f4b2cf150e80044a841b0fb820fcbc
mimseong <parkseongmin98@gmail.com>
2020-01-10 00:18:31 +0900

    feat: Add today's problem : 10952

---
+++ 10952.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    
+    while(scanf("%d %d", &a, &b)){
+        if(a==0 && b==0)
+            break;
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+}
