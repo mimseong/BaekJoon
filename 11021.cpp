ab9e2896fcc9dc5a1469eb5ed6715ba2a1ce0939
mimseong <parkseongmin98@gmail.com>
2020-01-11 12:26:19 +0900

    feat: Add today's problem : 11021

---
+++ 11021.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+using i64 = long long;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++){
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d\n", i, a+b);
+    }
+
+    return 0;
+}
