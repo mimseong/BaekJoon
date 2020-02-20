3e9941362f95291b7e159ab9c2aaa3f930e100d8
mimseong <parkseongmin98@gmail.com>
2020-02-20 14:21:29 +0900

    feat: Add today's problem : 15649

---
+++ 15649.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void    print_vector(int m, vector<int> v)
+{
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d ", v[i]);
+    }
+    printf("\n");
+}
+
+bool    promising(int index, vector<int> v)
+{
+    for (int i = 1; i <= index; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (v[i] == v[j])
+                return (false);
+        }
+    }
+    return (true);
+}
+
+void    find_sequence(int index, int n, int m, vector<int> v)
+{
+    if (index == m)
+        print_vector(m, v);
+    else
+    {
+        for (int i = 1; i <= n; )
+        {
+            v[index] = i++;
+            if (!promising(index, v))
+            {
+                continue;
+            }
+            find_sequence(index + 1, n, m, v);
+        }
+    }
+}
+
+int     main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    vector<int> v(m, 0);
+    
+    find_sequence(0, n, m, v);
+}
