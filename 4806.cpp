98035484a7a8fbbfc68c0e668616fc3fe4a35c05
mimseong <parkseongmin98@gmail.com>
2020-09-15 17:04:46 +0900

    feat: Add today's problem : 4806

---
+++ 4806.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <functional>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+
+#define xx first
+#define yy second
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+using i64 = long long;
+using ii = pair<int, int>;
+using ii64 = pair<i64, i64>;
+
+
+
+int     main()
+{
+    char s[101];
+    int ans = 0;
+
+    while (cin.getline(s, 100))
+    {
+        ans++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
