9dad1bef27cdf255f5cf4d7577a27a4a64afcc6a
mimseong <parkseongmin98@gmail.com>
2020-09-22 10:29:55 +0900

    feat: Add today's problem : 10814

--- 10814.cpp
+++ 10814.cpp
@@ -2,26 +2,45 @@
 #include <algorithm>
 #include <vector>
 #include <string>
-using namespace std;
+#include <functional>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <cstring>
+#include <bitset>
 
-bool compare(pair<int,string> a, pair<int,string> b){
-    return a.first < b.first;
+#define xx first
+#define yy second
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+using i64 = long long int;
+using ii = pair<int, int>;
+using ii64 = pair<i64, i64>;
+
+bool sortbyfirst(const pair<int, string>& a, const pair<int, string>& b)
+{
+    return (a.first < b.first);
 }
 
-int main() {
+int     main()
+{
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
-    
+
     int n;
     cin >> n;
-    vector <pair<int,string>> v(n);
 
-    for(int i = 0; i < n; i++){
-        cin >> v[i].first >> v[i].second;
-    }
-    stable_sort(v.begin(), v.end(), compare);
-    for(int i = 0; i < n; i++){
-        cout << v[i].first << " " << v[i].second << '\n';
-    }
+    vector<pair<int, string>> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i].xx >> v[i].yy;
+
+    stable_sort(all(v), sortbyfirst);
+    for (int i = 0; i < n; i++)
+        cout << v[i].xx << " " << v[i].yy << '\n';
+
     return 0;
 }
