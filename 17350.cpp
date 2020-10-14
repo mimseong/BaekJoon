6ef07678fd5b24865c3175e94cb6e98dd5dddb29
mimseong <parkseongmin98@gmail.com>
2020-10-14 12:11:16 +0900

    feat: Add today's problem : 17350

---
+++ 17350.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <functional>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#define xx first
+#define yy second
+#define all(x) (x).begin(), (x).end()
+#pragma warning(disable:4996)
+ 
+using namespace std;
+using i64 = long long;
+using ii = pair<int, int>;
+using ii64 = pair<i64, i64>;
+ 
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        string str;
+        cin >> str;
+        
+        if (str == "anj")
+        {
+            printf("뭐야;");
+            return 0;
+        }
+        
+    }
+    printf("뭐야?");
+    
+    return 0;
+}
