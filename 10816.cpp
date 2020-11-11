1616c3611514cac81e15b18d75d4eaef7c944dfa
mimseong <parkseongmin98@gmail.com>
2020-11-11 16:06:57 +0900

    feat: Add today's problem : 10816

--- 10816.cpp
+++ 10816.cpp
@@ -1,27 +1,50 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-using namespace std;
+#include <string>
+#include <functional>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <cstring>
+#include <bitset>
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    vector <int> v(n);
-    
-    for(int i = 0; i < n; i++)
-        scanf("%d", &v[i]);
-        
-    sort(v.begin(), v.end());
-    
-    int m;
-    scanf("%d", &m);
-    
-    for(int i = 0; i < m; i++){
-        int input;
-        scanf("%d", &input);
-        int tmp = upper_bound(v.begin(), v.end(), input) - lower_bound(v.begin(), v.end(), input);
-        printf("%d ", tmp);
-    }
-    
-    return 0;
+#define xx first
+#define yy second
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+using i64 = long long int;
+using ii = pair<int, int>;
+using ii64 = pair<i64, i64>;
+using iii = tuple<int, int, int>;
+
+int countv[20000005];
+
+int     main()
+{
+	int n;
+	scanf("%d", &n);
+
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &v[i]);
+
+	sort(all(v));
+
+	int m;
+	scanf("%d", &m);
+	for (int i = 0; i < m; i++)
+	{
+		int n;
+		scanf("%d", &n);
+
+		printf("%d ", upper_bound(all(v), n) - lower_bound(all(v), n));
+	}
+
+	
+	return 0;
 }
