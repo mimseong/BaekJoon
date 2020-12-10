1174329f21991a2ea798988e93fe35ecff0f030a
mimseong <parkseongmin98@gmail.com>
2020-12-10 23:14:20 +0900

    feat: Add today's problem : 14924

---
+++ 14924.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <functional>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <cstring>
+#include <bitset>
+
+#define xx first
+#define yy second
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+using i64 = long long int;
+using ii = pair<int, int>;
+using ii64 = pair<i64, i64>;
+using iii = tuple<int, int, int>;
+
+int     main()
+{
+	int s, d, t;
+	scanf("%d %d %d", &s, &t, &d);
+
+	printf("%d\n", d / (s * 2) * t);
+
+	return 0;
+}
