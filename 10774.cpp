ea3abb53695f2413710e0498178844d3a1ffb2f1
mimseong <parkseongmin98@gmail.com>
2020-12-24 08:53:30 +0900

    feat: Add today's problem : 10774

--- 10774.cpp
+++ 10774.cpp
@@ -5,66 +5,74 @@
 #include <functional>
 #include <string>
 #include <queue>
+#include <deque>
 #include <stack>
 #include <set>
 #include <map>
 #include <cmath>
 #include <cstring>
+#include <bitset>
 
 #define xx first
 #define yy second
 #define all(x) (x).begin(), (x).end()
 
 using namespace std;
-using i64 = long long;
+using i64 = long long int;
 using ii = pair<int, int>;
+using iis = pair<int, string>;
 using ii64 = pair<i64, i64>;
+using iii = tuple<int, int, int>;
 
 int     main()
 {
-    int k, l;
-    cin >> k >> l;
-    
-    vector<int> size(k + 5);
-    for (int i = 1; i <= k; i++)
-    {
-        string a;
-        cin >> a;
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-        if (a == "S")
-            size[i] = 0;
-        else if (a == "M")
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> size(n + 5);
+    for (int i = 1; i <= n; i++)
+    {
+        string s;
+        cin >> s;
+
+        if (s == "M")
             size[i] = 1;
-        else
+        else if (s == "S")
+            size[i] = 0;
+        else if (s == "L")
             size[i] = 2;
     }
 
-    vector<bool> pick(k + 5);
-    for (int i = 0; i < l; i++)
+    vector<bool> v(n + 5);
+    for (int i = 0; i < m; i++)
     {
-        string a;
-        int b, c;
-        cin >> a >> b;
+        string s;
+        int x;
 
-        if (a == "S")
-            c = 0;
-        else if (a == "M")
-            c = 1;
-        else
-            c = 2;
+        cin >> s >> x;
+        int size_t;
+        if (s == "M")
+            size_t = 1;
+        else if (s == "S")
+            size_t = 0;
+        else if (s == "L")
+            size_t = 2;
 
-        if (!pick[b] && c <= size[b])
-            pick[b] = true;
+        //cout << size_t << " " << size[x] << " " << v[x] << "\n";
+
+        if (!v[x] && size_t <= size[x])
+            v[x] = true;
     }
 
-    int ans = 0;
-    for (int i = 1; i <= k; i++)
+    int cnt = 0;
+    for (int i = 1; i <= n; i++)
     {
-        if (pick[i])
-            ans++;
+        if (v[i])
+            cnt++;
     }
-
-    cout << ans << endl;
+    cout << cnt << "\n";
 
     return 0;
 }
