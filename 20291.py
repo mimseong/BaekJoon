a78e10220bc2e11350b6a5a1cb3aab7c7da87e40
mimseong <parkseongmin98@gmail.com>
2025-10-18 10:33:16 +0900

    feat: Add today's problem 20291

---
+++ 20291.py
@@ -0,0 +1,27 @@
+def main():
+    n = int(input())
+
+    dict = {}
+    for i in range(n):
+        s = input()
+
+        dot_pos = s.find(".")
+        extension = s[dot_pos + 1 :]
+
+        if dict.get(extension) == None:
+            dict[extension] = 1
+        else:
+            dict[extension] += 1
+
+    keys = []
+    for key in dict:
+        keys.append(key)
+
+    keys.sort()
+
+    for key in keys:
+        print(f"{key} {dict[key]}")
+
+
+if __name__ == "__main__":
+    main()
